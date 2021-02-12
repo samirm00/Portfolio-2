801bd5a3c8412145c16a5cf11702430ca66feaf4
samirm00 <sammoham.be@gmail.com>
2021-02-12 20:01:24 +0100

    final check

--- src/handlers/handleProject.js
+++ src/handlers/handleProject.js
@@ -9,8 +9,11 @@ export function handleProject(){
 
   let ProjectUrl = "https://api.github.com/users/samirm00/repos" ;
 
-  const myProjectIds = [300421551 , 303137023,308762295, 302444542,320434177,322943999,332482701,334689597];
+  // Array of the selected project ids
 
+  const myProjectIds = [300421551 , 303137023,308762295, 302444542,320434177,322943999,332482701,334689597];
+ // declare a variable to be able to add it to the logger 
+  let project ;
   // filter data array 
  const filterData = (data) =>{
  data.forEach(element =>{
@@ -18,7 +21,7 @@ export function handleProject(){
      return ;
     }
 
-    let project = new Project(element.id, element.name, element.homepage) ;    
+     project = new Project(element.id, element.name, element.homepage) ;    
     const renderProject = project.render();
 
     const projectsDiv = document.getElementById('projects');
@@ -28,11 +31,20 @@ export function handleProject(){
 
  }
 
+ // fetch data form GitHub API 
+ 
   fetch(ProjectUrl)
   .then(res => res.json())
   .then(data =>  filterData(data))
   .catch(err => console.log(err)); 
-   
+  
+  
+//posh project to logger function 
+  logger.push({
+    action: 'fetch project',
+    project: project,
+    
+})
 }
 
 
